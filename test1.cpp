79ff858a524503e05dbd6b88180e2131d2af70ee
bundlelife <bundlelife@empal.com>
2018-07-19 10:25:21 +0900

    Init repository

---
+++ test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	
+	cout << argv[0] <<  "\n";
+	
+	cout << "Hello World!" << "\n";
+
+
+	return 0;
+	
+}
---
+++ test2.cpp
@@ -0,0 +1,50 @@
+#include <typeinfo>		// for typeid
+#include <functional>	// for std::cref()
+#include <string>
+#include <iostream>
+
+void printString(std::string const& s)
+{
+  std::cout << s << '\n';
+}
+
+template <typename T>
+void printT (T arg)
+{
+  printString(arg);         // might convert arg back to std::string
+}
+
+template <typename T>
+T const& max (T const& a, T const & b)
+{
+	return a > b ? a : b;
+}
+
+// template <typename T>
+// T max (T a, T b)
+// {
+// 	return a > b ? a : b;
+// }
+
+template <typename T, typename U>
+auto max (T a, U b)
+{
+	return a > b ? a : b;
+}
+
+int main()
+{
+  std::string s = "hello";
+  auto value = ::max(3, 8);
+  std::cout <<  "value:" << value << " type:" << typeid(value).name() << std::endl;
+
+  printT(s);               // print s passed by value
+  printT(std::cref(s));    // print s passed “as if by reference”
+}
+
+
+
+
+
+
+
